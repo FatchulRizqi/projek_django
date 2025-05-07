eccc773ecab9e8b154b5e5e1cca4cd32d1372785
FatchulRizqi <mochfatchulhrizqi@gmail.com>
2025-05-07 15:50:51 +0700

    mempelajari penggunaan url pada app

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
 from . import views
-
+from contact import views as contactViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.welcome, name='welcome'),
-    path('about/', views.about, name='home'),
-    path('contact/', views.contact, name='contact'),
+    path('about/', include("about.urls")),
+    path('contact/', contactViews.contact, name='contact'),
 ]
--- website/website/views.py
+++ website/website/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
+from django.conf.urls import include
 from django.shortcuts import render
 
 def welcome(request):
     return render(request,'index.html')
 
-def about(request):
-    return render(request,'about.html')
+# def about(request):
+#     return render(request,include('about.urls'))
 
 def contact(request):
     return render(request, 'kontak.html')
